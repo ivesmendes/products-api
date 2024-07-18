255ef8f8d4fcf6f65181f510b2deaa43ed2d1df2
ivesmedes <ivesmedes@gmail.com>
2024-07-18 11:33:39 -0300

    Implementação do método Post no controlador

--- src/main/java/com/api/products/controllers/ProductController.java
+++ src/main/java/com/api/products/controllers/ProductController.java
@@ -1,7 +1,15 @@
 package com.api.products.controllers;
 
+import com.api.products.dtos.ProductRecordDto;
+import com.api.products.models.ProductModel;
 import com.api.products.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +17,12 @@ public class ProductController {
 
     @Autowired
     ProductRepository productRepository;
+
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto){
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
 
 }
