00e2d2dd7c085a7109bf4547928ab8262655e1fd
ivesmedes <ivesmedes@gmail.com>
2024-07-18 09:03:00 -0300

    repositório adicionado

---
+++ src/main/java/com/api/products/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.api.products.repositories;
+
+import com.api.products.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+}
