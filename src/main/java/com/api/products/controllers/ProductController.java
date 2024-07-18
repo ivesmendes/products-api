f265e934bb88b2fca06ab7c60e1de94115522493
ivesmedes <ivesmedes@gmail.com>
2024-07-18 09:29:43 -0300

    Product controller adicionado

---
+++ src/main/java/com/api/products/controllers/ProductController.java
@@ -0,0 +1,13 @@
+package com.api.products.controllers;
+
+import com.api.products.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    ProductRepository productRepository;
+
+}
